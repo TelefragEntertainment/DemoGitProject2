480c7706caf81fc7ac9c6b734713f20dbf1f83f9
Blake <cblakequ@gmail.com>
2023-10-11 15:39:24 -0400

    Added rotation

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    // Update is called once per frame HELLo
     void Update()
     {
         transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime, 0, 0);
+        transform.Rotate(new Vector3(0, 0, Time.deltaTime));
     }
 }
